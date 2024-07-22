c7703e791465a6e95b0d0b48c5e8be4867d21b16
VishGK008 <vishwasgk999@gmail.com>
2024-07-22 14:53:41 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dashboard = document.getElementById('dashboard');
+    dashboard.innerHTML += '<p>Welcome to your dashboard!</p>';
+});
